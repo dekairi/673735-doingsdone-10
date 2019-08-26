af9f09aa9df2edec273cc83c35780543bc2a1b5f
Irina Kovalchuk <synthbeing@icloud.com>
2019-08-26 14:17:04 +0300

    Fixes

--- queries.sql
+++ queries.sql
@@ -23,53 +23,47 @@
 # Dump of table projects
 # ------------------------------------------------------------
 
-LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
 
-INSERT INTO `projects` (`id`, `title`, `user`)
+INSERT INTO `projects` (`title`, `user`)
 VALUES
-	(0,'Входящие',0),
-	(1,'Учеба',1),
-	(2,'Работа',0),
-	(3,'Домашние дела',0),
-	(4,'Авто',1);
+	('Входящие',0),
+	('Учеба',1),
+	('Работа',0),
+	('Домашние дела',0),
+	('Авто',1);
 
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Dump of table task
 # ------------------------------------------------------------
 
-LOCK TABLES `task` WRITE;
 /*!40000 ALTER TABLE `task` DISABLE KEYS */;
 
-INSERT INTO `task` (`id`, `date_created`, `status`, `title`, `file`, `date_todo`, `project`, `user`)
+INSERT INTO `task` (`date_created`, `status`, `title`, `file`, `date_todo`, `project`, `user`)
 VALUES
-	(0,'2017-08-19 00:00:00',0,'Собеседование в IT компании',NULL,'2019-12-01 00:00:00',2,0),
-	(1,'2018-08-01 00:00:00',0,'Выполнить тестовое задание',NULL,'2019-08-24 00:00:00',2,0),
-	(2,'2018-08-01 00:00:00',1,'Сделать задание первого раздела',NULL,'2019-12-16 00:00:00',1,1),
-	(3,'2019-08-01 00:00:00',0,'Встреча с другом',NULL,'2019-12-22 00:00:00',0,0),
-	(4,'2019-08-02 00:00:00',0,'Купить корм для кота\n',NULL,NULL,3,0),
-	(5,'2019-08-04 00:00:00',0,'Заказать пиццу',NULL,NULL,3,0);
+	('2017-08-19 00:00:00',0,'Собеседование в IT компании',NULL,'2019-12-01 00:00:00',2,0),
+	('2018-08-01 00:00:00',0,'Выполнить тестовое задание',NULL,'2019-08-24 00:00:00',2,0),
+	('2018-08-01 00:00:00',1,'Сделать задание первого раздела',NULL,'2019-12-16 00:00:00',1,1),
+	('2019-08-01 00:00:00',0,'Встреча с другом',NULL,'2019-12-22 00:00:00',0,0),
+	('2019-08-02 00:00:00',0,'Купить корм для кота\n',NULL,NULL,3,0),
+	('2019-08-04 00:00:00',0,'Заказать пиццу',NULL,NULL,3,0);
 
 /*!40000 ALTER TABLE `task` ENABLE KEYS */;
-UNLOCK TABLES;
 
 
 # Dump of table user
 # ------------------------------------------------------------
 
-LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 
-INSERT INTO `user` (`id`, `register_date`, `email`, `name`, `password`)
+INSERT INTO `user` (`register_date`, `email`, `name`, `password`)
 VALUES
-	(0,'2016-06-19 00:00:00','someemail@somedomain.com','Ирина','1234'),
-	(1,'2017-08-04 00:00:00','123@123.123','Дмитрий','5677');
+	('2016-06-19 00:00:00','someemail@somedomain.com','Ирина','1234'),
+	('2017-08-04 00:00:00','123@123.123','Дмитрий','5677');
 
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
 
 # Get projects of user 0
 SELECT * FROM `projects` WHERE `user` = '0' LIMIT 0,1000;
